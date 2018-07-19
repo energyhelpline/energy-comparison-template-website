6da96d5d83a7847d40c85efc5e1eba1619d78313
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2018-07-19 11:44:21 +0100

    Release version 1.0.3

--- Publish.ps1
+++ Publish.ps1
@@ -9,6 +9,21 @@ function GetNextTag($currentTag)
     return "$textBeforeLastFullStop.$incrementedBuildNumber"
 }
 
+function ApplyPatch($tag)
+{
+    git apply ..\changesSinceLastTag.patch
+    git add .
+    git reset .\BareboneUi\appsettings.json
+    git commit -m "Release version $tag"
+    git checkout .
+    git tag -a $tag -m "Release version $tag"
+}
+
+function ReclonePublicRepo{
+    Remove-Item .\barebone-ui-public -Recurse -Force
+    git clone git@github.com:energyhelpline/barebone-ui-public.git
+}
+
 if ([System.IO.File]::Exists("changesSinceLastTag.patch"))
 {
     Remove-Item changesSinceLastTag.patch
@@ -29,14 +44,11 @@ git tag -a $newTag -m "Release version $newTag"
 git diff $latestTag $newTag | Out-File -encoding ASCII changesSinceLastTag.temp.patch
 ((Get-Content .\changesSinceLastTag.temp.patch) -join "`n")  | Out-File -encoding ASCII changesSinceLastTag.patch
 
-Remove-Item .\barebone-ui-public -Recurse -Force
-git clone git@github.com:energyhelpline/barebone-ui-public.git
+ReclonePublicRepo
 Push-Location .\barebone-ui-public
-git apply ..\changesSinceLastTag.patch
-git add .
-git reset .\BareboneUi\appsettings.json
-git commit -m "Release version $newTag"
-git tag -a $newTag -m "Release version $newTag"
-git checkout .
+ApplyPatch $newTag
 git push --follow-tags
 Pop-Location
+
+# git push tags on private repo
+git push --tags
